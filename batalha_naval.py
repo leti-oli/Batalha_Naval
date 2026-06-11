6e4fb9ec956fa7f61ea92840674790d657ae0555
leti-oli <o.leticiaa29@gmail.com>
2026-06-11 12:38:27 -0300

    mudanças

--- batalha_naval.py
+++ batalha_naval.py
@@ -1,37 +1,63 @@
 import random
-import os
+import os 
+# random -> para gerar números aleatórios para o computador escolher as
+#  posições das embarcações e os ataques
+# os -> para limpar a tela do terminal, deixando a interface mais limpa e fácil de ler
 
 # VARIAVEIS
 
 embarcacoes_C = 5
 embarcacoes_J = 5
+#qnt de emnbarcações de cada jogador
 jogadas_J = 0
 jogadas_C = 0
+#qnnt de jogadas
 gabarito_J = []
 gabarito_C = []
+#istas para armazenar as posições das embarcações 
 
-# FUNÇÕES
 
+
+# FUNÇÕES   
+# menu e explicação
+def menu():
+    print("Bem-vindo ao Batalha Naval!⛵⚔️")
+    print("O objetivo do jogo é afundar as embarcações do adversário.")
+    print("Cada jogador tem 5 embarcações para posicionar no tabuleiro.")
+    print("O tabuleiro é composto por 5 linhas (0 a 4) e 10 colunas (0 a 9).")
+    print("Na sua vez, você deve escolher as coordenadas (X e Y) para atacar o adversário.")
+    print("O jogo termina quando um dos jogadores afunda todas as embarcações do adversário.")
+    print("Boa sorte!⚔️\n")
+    continuar = input(f"Aperte enter para continuar.")
+    os.system('cls')
+
+
+# função para criar as matrizes do jogo, representando os tabuleiros dos jogadores e do computador
 def criar_matriz(linhas, colunas):
     matriz = []
     for i in range(linhas):
-        matriz.append([0] * colunas)
+        matriz.append(["🟦"] * colunas)
     return matriz
 
-def print_matriz(matriz):
-    for linha in matriz:
-        print(linha)
 
+# função para imprimir as matrizes do jogo, mostrando o estado atual dos tabuleiros dos jogadores e do computador
+def print_matriz(matriz):
+
+    for linha in matriz:
+        print(" ".join(linha))
+
+#função para imprimir os tabuleiros dos jogadores e do computador, mostrando as posições das embarcações, os acertos e os erros e  embarcações restantes de cada jogador
 def print_tabuleiro():
     print("Tabuleiro do Jogador")
     print_matriz(m_J)
-    print(f"Gabarito Jogador: {gabarito_J}")
+    # print(f"Gabarito Jogador: {gabarito_J}")
     print(f"Embarcações Restantes: {embarcacoes_J}")
     print(f"\nTabuleiro do Computador")
     print_matriz(m_C)
-    print(f"Gabarito Computador: {gabarito_C}")
+    # print(f"Gabarito Computador: {gabarito_C}")
     print(f"Embarcações Restantes: {embarcacoes_C}")
 
+#função para verificar se algum dos jogadores venceu o jogo
 def verificar_vitoria(embarcacoes_J, embarcacoes_C):
     if embarcacoes_J == 0:
         os.system('cls')
@@ -50,6 +76,7 @@ def verificar_vitoria(embarcacoes_J, embarcacoes_C):
     
     return False
 
+
 # MATRIZES
 
 m_J = criar_matriz(5,10)
@@ -57,11 +84,14 @@ m_C = criar_matriz(5,10)
 m_JG = criar_matriz(5,10)
 m_CG = criar_matriz(5,10)
 
+
+menu()
+
 while True:
 
     # LOOP POSICIONAMENTO JOGADOR
 
-    while jogadas_J != 5:
+    while jogadas_J != 5:   
         
         print(f"Seu tabuleiro")
         print_matriz(m_JG)
@@ -88,8 +118,9 @@ while True:
                     continue
 
                 if y in (range(0,10)):           
-                    if m_JG[x][y] != "X":
-                        m_JG[x][y] = "X"
+                    if m_JG[x][y] != "⛵":
+                        m_JG[x][y] = "⛵"
+                        m_J[x][y] = "⛵"
                         jogadas_J += 1
                         gabarito_J.append(str(x) + str(y))
                         os.system('cls')
@@ -122,8 +153,9 @@ while True:
 
         x = random.randint(0, 4)
         y = random.randint(0, 9)
-        if m_CG[x][y] != "X":    
+        if m_CG[x][y] == "🟦":    
             m_CG[x][y] = "X"
+            
             jogadas_C += 1
             gabarito_C.append(str(x) + str(y))
 
@@ -163,6 +195,8 @@ while True:
                         if m_CG[x][y] == "X":
                             os.system('cls')
                             embarcacoes_C -= 1
+                            m_C[x][y] = "💥"
+                            m_CG[x][y] = "💥"
                             print(f"Jogador acertou!\n")
                             print_tabuleiro()
                             continuar = input(f"\nAperte enter para continuar.")
@@ -170,6 +204,7 @@ while True:
                             break
                         else:
                             os.system('cls')
+                            m_C[x][y] = "🌊"
                             print(f"Jogador errou.\n")
                             print_tabuleiro()
                             continuar = input(f"\nAperte enter para continuar.")
@@ -199,14 +234,17 @@ while True:
 
         x = random.randint(0, 4)
         y = random.randint(0, 9)
-
+    
         print(f"\nComputador escolheu X: {x}")
         print(f"Computador escolheu Y: {y}")
         continuar = input(f"\nAperte enter para continuar.")
 
-        if m_JG[x][y] == "X":
+        if m_JG[x][y] == "⛵":
+        
             os.system('cls')
             embarcacoes_J -= 1
+            m_J[x][y] = "💥"
+            m_JG[x][y] = "💥"
             print(f"Computador acertou!\n")
             print_tabuleiro()
             continuar = input(f"\nAperte enter para continuar.")
@@ -214,11 +252,12 @@ while True:
       
         else:
             os.system('cls')
+            m_J[x][y] = "🌊"
             print(f"Computador errou.\n")
             print_tabuleiro()
             continuar = input(f"\nAperte enter para continuar.")
             os.system('cls')
-        
+
         # VERIFICAR VITÓRIA COMPUTADOR
 
         if verificar_vitoria(embarcacoes_J,embarcacoes_C):
